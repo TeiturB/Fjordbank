387c6ef2fb8f156acac52c72b8e497154899558e
SleightlySly <edvinarge@gmail.com>
2023-03-09 06:37:01 +0000

    Clean up

--- helpers.py
+++ helpers.py
@@ -1,25 +1,7 @@
-import os
-import requests
-import urllib.parse
 import locale
 
-from flask import redirect, render_template, request, session
+from flask import redirect, session
 from functools import wraps
-
-
-def apology(message, code=400):
-    """Render message as an apology to user."""
-    def escape(s):
-        """
-        Escape special characters.
-
-        https://github.com/jacebrowning/memegen#special-characters
-        """
-        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
-                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
-            s = s.replace(old, new)
-        return s
-    return render_template("apology.html", top=code, bottom=escape(message)), code
 
 
 def login_required(f):
@@ -36,43 +18,27 @@ def login_required(f):
     return decorated_function
 
 
-def lookup(symbol):
-    """Look up quote for symbol."""
-
-    # Contact API
-    try:
-        api_key = os.environ.get("API_KEY")
-        url = f"https://cloud.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}"
-        response = requests.get(url)
-        response.raise_for_status()
-    except requests.RequestException:
-        return None
-
-    # Parse response
-    try:
-        quote = response.json()
-        return {
-            "name": quote["companyName"],
-            "price": float(quote["latestPrice"]),
-            "symbol": quote["symbol"]
-        }
-    except (KeyError, TypeError, ValueError):
-        return None
-
-
-# def dkk(value):
-    # """Format value as DKK."""
-    # return f"${value:,.2f}"
-
-# def dkk(value):
-#     """Format value as DKK."""
-#     return f"{value:,.2f}kr"
-
-
 def dkk(value):
     """Format value as DKK."""
     locale.setlocale(locale.LC_ALL, 'da_DK.UTF-8')
     return f"{locale.format_string('%.2f', value, grouping=True):>10}"
 
+
 # Another way to achieve similar results
     # return f"{value:,.2f}".replace(",", ";").replace(".", ",").replace(";", ".") + "kr"
+
+
+# Deprecated. Saved as a potential 404 screen in the future
+# def apology(message, code=400):
+#     """Render message as an apology to user."""
+#     def escape(s):
+#         """
+#         Escape special characters.
+
+#         https://github.com/jacebrowning/memegen#special-characters
+#         """
+#         for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+#                          ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+#             s = s.replace(old, new)
+#         return s
+#     return render_template("apology.html", top=code, bottom=escape(message)), code
